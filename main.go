2ebe41e5e7ae4365e4bf78088490d7bbd89098fd
hararudoka <jaroldbulter@gmail.com>
2022-01-29 07:52:59 +0300

    cicd

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 	"os"
 	"shorter/internal/handler"
 	"shorter/internal/html"
@@ -27,12 +28,12 @@ func main() {
 
 	h.REGISTER(*e.Group(""), &handler.Shorts{})
 
-	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+	//e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 
-	//pem := os.Getenv("SERVER_PEM")
-	//key := os.Getenv("SERVER_KEY")
-	//
-	//if err := e.StartTLS(":"+os.Getenv("PORT"), []byte(pem), []byte(key)); err != http.ErrServerClosed {
-	//	panic(err)
-	//}
+	pem := os.Getenv("SERVER_PEM")
+	key := os.Getenv("SERVER_KEY")
+
+	if err := e.StartTLS(":"+os.Getenv("PORT"), []byte(pem), []byte(key)); err != http.ErrServerClosed {
+		panic(err)
+	}
 }
